c75fa4b777a5b86c8b257263dfe44056fcd28177
Adria Montoto <75563346+adriamontoto@users.noreply.github.com>
2025-09-12 19:40:53 +0200

    feat: implement event bus and in memory event bus
    Signed-off-by: Adria Montoto <75563346+adriamontoto@users.noreply.github.com>

--- domain_event_pattern/__init__.py
+++ domain_event_pattern/__init__.py
@@ -1,10 +1,13 @@
-__version__ = '0.1.0'
+__version__ = '0.2.0'
 
+from .buses import EventBus, InMemoryEventBus
 from .decorators import handle_events
 from .models import DomainEvent, DomainEventHandler
 
 __all__ = (
     'DomainEvent',
     'DomainEventHandler',
+    'EventBus',
+    'InMemoryEventBus',
     'handle_events',
 )
